fe23a1dfd3605d85a11cdeb7f0762eca1400246b
vhvalbone <albin06svensson@gmail.com>
2024-04-03 10:54:30 +0200

    Add project files.

---
+++ inläm-kon/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inlämning_kon
+{
+    class bil
+    {
+        public string märke;
+        public string model;
+        public string tillverkare;
+        public string vikt;
+        public string pris;
+
+        public void Metod1()
+        {
+            Console.WriteLine("hej här är lite fakta om fordonet:" + märke + model + tillverkare + vikt + pris);
+        }
+
+        public string Märkee;
+        public string modell;
+        public string tillverkaree;
+        public string viktt;
+        public string priss;
+
+        public void Metod2()
+        {
+            Console.WriteLine("Hej är är lite fakta om en annan bil"+Märkee + modell + tillverkaree + viktt + priss);
+        }
+    }
+}
+
---
+++ inläm-kon/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace inlämning_kon
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            bil p1 = new bil();
+            p1.märke = " Märket är alfa romro ";
+            p1.model = " modelen är alfa romeo 4c ";
+            p1.tillverkare = " tillverkaren är Alfa romeo ";
+            p1.vikt = "väger runt 895 kg ";
+            p1.pris = "kosta ifrån 460 000 kr men kan bli dyrare ";
+
+            p1.Metod1();
+            
+                bil p2 = new bil();
+                p2.Märkee = "märket är ford ";
+                p2.modell = " modelen är ford focus";
+                p2.tillverkaree = "tillverkaren är ford";
+                p2.viktt = " kan varieras ifrån 1349-1563 kg";
+                p2.priss = " kan kosta ifårn 380 000 kr till mera";
+
+                p2.Metod2();
+            
+        }
+    }
+}
